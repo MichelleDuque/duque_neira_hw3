fe0ef651255248f2e29ccdab3934dad580acf278
MichelleDuque <michelled_1994@hotmail.com>
2024-03-26 18:35:15 -0400

    created slider functions using array and index with count

--- js/main.js
+++ js/main.js
@@ -1,58 +1,84 @@
 const digimon = Vue.createApp({
-    created(){
-        console.log("created lifecycle hook called");
+    created() {
+        // Fetch characters data from the API
         fetch("http://localhost/digimon/public/characters")
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            this.charactersData = data;
-        })
-        .catch(error => {
-            console.error(error);
-        })
+            .then(res => res.json())
+            .then(data => {
+                this.charactersData = data;
+                this.displayCharacter(0); // Display the first character
+            })
+            .catch(error => {
+                console.error(error);
+            });
     },
-
-    data(){
-        return{
+    
+    data() {
+        return {
             charactersData: [],
+            count: 0,
             levels: "",
-            fields: [], 
+            fields: [],
             types: "",
             attributes: "",
             photo: "",
-            error: "" 
+            error: ""
         }
     },
 
     methods: {
-        getDigimon(whichDigimon){
-            console.log(whichDigimon);
+        // Method to display a character
+        displayCharacter(count) {
+            const character = this.charactersData[count];
+            this.getDigimon(character.name);
+        },
+
+        // Method to fetch exteran API
+        getDigimon(whichDigimon) {
             let title = whichDigimon;
             let convertedTitle = title.split(" ").join("+");
-            console.log(convertedTitle);
 
             fetch(`https://digi-api.com/api/v1/digimon/${convertedTitle}`)
-            .then(res => res.json())
-            .then(data =>{
-                console.log(data)
-                if(data){
-                    console.log(data);
-                    const digimon = data;
-                    this.error = false;
-                    this.levels = data.levels ? data.levels.map(lev => lev.level).join(", ") : "Not available";                     
-                    this.fields = data.fields ? data.fields.map(fie => fie.image.replace(/%20/g, " ")) : [];            
-                    this.types = digimon.types ? digimon.types[0].type :  "Not available";
-                    this.attributes = data.attributes ? data.attributes.map(attr => attr.attribute).join(", ") : "Not available";                
-                } else {
-                    this.error = "No digimon found. Try Again."
-                }
-            })
-            .catch(error => {
-                console.error(error);
-                this.error = "Error fetching digimon data"; 
-            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data) {
+                        const digimon = data;
+                        this.error = false;
+                        this.levels = data.levels ? data.levels.map(lev => lev.level).join(", ") : "Not available";
+                        this.fields = data.fields ? data.fields.map(fie => fie.image) : "Not available";                        
+                        this.types = digimon.types ? digimon.types[0].type : "Not available";
+                        this.attributes = data.attributes ? data.attributes.map(attr => attr.attribute).join(", ") : "Not available";
+                    } else {
+                        this.error = "No digimon found. Try Again."
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                    this.error = "Error fetching digimon data";
+                })
+        },
+
+        //SLIDER FUNCTIONS
+
+        // Method to display the next character
+        nextDigimon() {
+            this.count++;
+            if (this.count >= this.charactersData.length) {
+                this.count = 0;
+            }
+            this.displayCharacter(this.count);
+        },
+
+        // Method to display the previous character
+        previousDigimon() {
+            this.count--;
+            if (this.count < 0) {
+                this.count = this.charactersData.length - 1;
+            }
+            this.displayCharacter(this.count);
         }
     }
 });
 
 digimon.mount("#app");
+
+
